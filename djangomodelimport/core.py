280be80f5cf1b034bcd251e86091cbfe130770d4
Aidan Lister <aidan@aidanlister.com>
2017-05-01 20:28:29 +1000

    Move the results class into core

--- djangomodelimport/core.py
+++ djangomodelimport/core.py
@@ -2,7 +2,6 @@ import tablib
 from django.forms import modelform_factory
 
 from .caches import SimpleDictCache
-from .results import ImportResultRow, ImportResultSet
 
 
 class ModelImporter:
@@ -58,3 +57,55 @@ class ModelImporter:
             importresult.append(i, row, errors, instance)
 
         return importresult
+
+
+class ImportResultSet:
+    """ Hold all imported results. """
+    import_headers = None
+    results = None
+
+    def __init__(self, import_headers):
+        self.results = []
+        self.import_headers = import_headers
+
+    def __str__(self):
+        return '{} {}'.format(self.import_headers, self.results)
+
+    def append(self, index, row, errors, instance):
+        self.results.append(
+            ImportResultRow(self, index, row, errors, instance)
+        )
+
+    def get_import_headers(self):
+        return self.import_headers
+
+    def get_results(self):
+        return self.results
+
+    def get_errors(self):
+        return [(row.linenumber, row.errors) for row in self.results if not row.is_valid()]
+
+
+class ImportResultRow:
+    """ Hold the result of an imported row. """
+    resultset = None
+    linenumber = None
+    row = None
+    errors = None
+    instance = None
+
+    def __init__(self, resultset, linenumber, row, errors, instance):
+        self.resultset = resultset
+        self.linenumber = linenumber
+        self.row = row
+        self.errors = errors
+        self.instance = instance
+
+    def __str__(self):
+        return '{} {}'.format(self.linenumber, self.row)
+
+    def get_instance_values(self):
+        return [getattr(self.instance, header) for header in self.resultset.import_headers]
+
+    def is_valid(self):
+        return len(self.errors) == 0
