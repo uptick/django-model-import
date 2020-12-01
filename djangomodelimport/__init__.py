152a4d226bc70c8db13057142d02f307fdcf8ec2
Jarek Głowacki <jarekwg@gmail.com>
2020-12-01 18:03:26 +1100

    0.4.3

--- djangomodelimport/__init__.py
+++ djangomodelimport/__init__.py
@@ -10,6 +10,6 @@ from .forms import ImporterModelForm  # noqa
 from .parsers import BaseImportParser, TablibCSVImportParser, TablibXLSXImportParser  # noqa
 from .widgets import CompositeLookupWidget  # noqa
 
-VERSION = (0, 4, 2)
+VERSION = (0, 4, 3)
 
 __version__ = '.'.join(str(x) for x in VERSION[:(2 if VERSION[2] == 0 else 3)])  # noqa
