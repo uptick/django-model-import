c29c217bc1f09b4827d341303f91fd240fc7038f
Aidan Lister <aidan@uptickhq.com>
2021-07-30 09:47:29 +1000

    bump version to 0.4.5

--- djangomodelimport/__init__.py
+++ djangomodelimport/__init__.py
@@ -10,6 +10,6 @@ from .forms import ImporterModelForm  # noqa
 from .parsers import BaseImportParser, TablibCSVImportParser, TablibXLSXImportParser  # noqa
 from .widgets import CompositeLookupWidget  # noqa
 
-VERSION = (0, 4, 4)
+VERSION = (0, 4, 5)
 
 __version__ = '.'.join(str(x) for x in VERSION[:(2 if VERSION[2] == 0 else 3)])  # noqa
