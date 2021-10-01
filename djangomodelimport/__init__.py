ba334382f523a99b2c0cf26205686decb33d19c3
Jarek Głowacki <jarekwg@gmail.com>
2021-10-02 00:19:33 +1000

    pop in another noqa

--- djangomodelimport/__init__.py
+++ djangomodelimport/__init__.py
@@ -8,7 +8,7 @@ from .fields import (  # noqa
 )
 from .forms import ImporterModelForm  # noqa
 from .parsers import BaseImportParser, TablibCSVImportParser, TablibXLSXImportParser  # noqa
-from .resultset import ImportResultRow, ImportResultSet
+from .resultset import ImportResultRow, ImportResultSet  # noqa
 from .widgets import CompositeLookupWidget  # noqa
 
 __version__ = '0.4.12'
