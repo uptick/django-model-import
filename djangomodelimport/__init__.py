86380a038d7ed48c0cd31de285d411cfc594a9d4
Jarek Głowacki <jarekwg@gmail.com>
2022-10-03 23:13:49 +1100

    Fields and widgets are no longer lazily-importable

--- djangomodelimport/__init__.py
+++ djangomodelimport/__init__.py
@@ -1,14 +1,6 @@
 from .core import ModelImporter  # noqa
-from .fields import (  # noqa
-    CachedChoiceField,
-    DateTimeParserField,
-    FlatRelatedField,
-    JSONField,
-    PreloadedChoiceField
-)
 from .forms import ImporterModelForm  # noqa
 from .parsers import BaseImportParser, TablibCSVImportParser, TablibXLSXImportParser  # noqa
 from .resultset import ImportResultRow, ImportResultSet  # noqa
-from .widgets import CompositeLookupWidget  # noqa
 
 __version__ = '0.4.18'
